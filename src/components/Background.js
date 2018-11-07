b42c062f7e1b30a7633a0a5efe323c84ba6c7cc8
Rolf Oldenkotte <info@rolfoldenkotte.nl>
2018-11-07 13:14:12 +0100

    Made backgrounds responsive

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 
+import Etheria from '../images/backgrounds/etheria.png';
 import Spacewindow from '../images/backgrounds/spacewindow.png';
 import Sandena from '../images/backgrounds/sandena.png';
 import Senate from '../images/backgrounds/senate.png';
@@ -10,25 +11,70 @@ import Vulcanus from '../images/backgrounds/vulcanus.png';
 
 class Background extends Component {
 
+    count = 0;
     state = {
         imgUrl: null
     }
 
-    componentDidMount() {
+    componentDidUpdate() {
         this.handleChange();
     }
 
     handleChange = () => {
-        if (this.props.location === "appartment") {
+        
+        if (this.props.location === "appartment" && this.count !== 1 ) {
             this.setState({
                 imgUrl: <img src={Spacewindow} alt={Spacewindow} className="background"/>
               })
+            this.count = 1;
             
         };
-        if (this.props.location === "senate") {
+        if (this.props.location === "senate" && this.count !== 2) {
             this.setState({
                 imgUrl: <img src={Senate} alt={Senate} className="background"/>
               })  
+            this.count = 2;
+        }
+
+        if (this.props.location === "sandena" && this.count !== 3) {
+            this.setState({
+                imgUrl: <img src={Sandena} alt={Sandena} className="background"/>
+              })  
+            this.count = 3;
+        }
+
+        if (this.props.location === "space" && this.count !== 4) {
+            this.setState({
+                imgUrl: <img src={Space} alt={Space} className="background"/>
+              })  
+            this.count = 4;
+        }
+
+        if (this.props.location === "spacecontrols" && this.count !== 5) {
+            this.setState({
+                imgUrl: <img src={Spacecontrols} alt={Spacecontrols} className="background"/>
+              })  
+            this.count = 5;
+        }
+
+        if (this.props.location === "unsinkable" && this.count !== 6) {
+            this.setState({
+                imgUrl: <img src={Unsinkable} alt={Unsinkable} className="background"/>
+              })  
+            this.count = 6;
+        }
+
+        if (this.props.location === "vulcanus" && this.count !== 7) {
+            this.setState({
+                imgUrl: <img src={Vulcanus} alt={Vulcanus} className="background"/>
+              })  
+            this.count = 7;
+        }
+        if (this.props.location === "etheria" && this.count !== 8) {
+            this.setState({
+                imgUrl: <img src={Etheria} alt={Etheria} className="background"/>
+              })  
+            this.count = 8;
         }
     }
 
