8451fe1d789b22b70a070f160476dad7126f266f
Rolf Oldenkotte <info@rolfoldenkotte.nl>
2019-01-20 14:20:12 +0100

    fix for pull request

--- src/components/parent/Tutorial.js
+++ src/components/parent/Tutorial.js
@@ -8,7 +8,7 @@ class Tutorial extends Component {
 	}
 
 	state = {
-		tutorialText: ["Vivamus molestie, neque vitae pellentesque porta, nulla nulla volutpat arcu, a feugiat lorem nunc eu dolor. Sed laoreet dictum suscipit. Maecenas non purus ultricies, pretium magna nec, pulvinar nisi. Nullam vitae magna fermentum, mollis purus eu, iaculis urna. Nullam luctus, purus vel finibus ornare, orci neque ultricies quam, id consectetur odio nisl suscipit lectus. Praesent pretium convallis aliquet.", "urabitur posuere fermentum tortor non semper. Quisque ut sem scelerisque, pharetra eros a, maximus metus. Mauris nec mauris tellus. Aenean vel eros sed mauris efficitur posuere eget in mauris. Nullam vestibulum euismod ex, at viverra justo tincidunt a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.", "And here are some last things."],
+		tutorialText: ["Voordat we beginnen vertellen we kort het een en ander over de website waarop jij het avontuur volgt: Boven deze tekst vind je de “Missie Controle”. Dit is een plaats waar je de vooruitgang van jouw kind in het verhaal kan volgen. Je navigeert door middel van de linker en rechter knoppen (voor- en achteruit) en de middelste knop zet het spel op pauze. Veeg naar links om verder te gaan.", "Het pagina symbool  brengt jou naar een overzicht van alle missies. Hier kun je uitgebreide beschrijvingen en informatie vinden over alle missies en ook over de desbetreffende voorbereidingen voor elke missie. (let op: deze pagina is momenteel nog niet functioneel)", "Het icoontje met de ‘?’ brengt jou naar een pagina met veelgestelde vragen. (let op: deze pagina is momenteel nog niet functioneel maar momenteel staat hier een nummer vermeld waar je contact mee kan opnemen bij vragen en/of problemen)", "Het tandwiel brengt jou op de pagina vol instellingsmogelijkheden. Hier kun je onder andere het geluid dempen en de taal aanpassen.", "Indien je het nog niet hebt gedaan raden wij jou aan om het speelboek door te lezen. Maak je klaar om de wereld te redden!"],
 		currentText: null,
 		counter: 0,
 	}
@@ -55,7 +55,7 @@ class Tutorial extends Component {
 					onSwipedUp={this.onSwipeUpListener}
 					onSwipe={this.onSwipeListener}>
 					<div className="tutorial-name"><p>Welcome</p></div>
-    					<p>Welcome to Family Quest</p>
+    					<p>Welkom bij Family Quest</p>
 						<p>{this.state.currentText}</p>
 
     			</Swipe>
